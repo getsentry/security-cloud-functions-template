e9efe70faa035bc2e8b9246e0412b0c74574db16
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2024-08-29 11:20:10 -0700

    clean up the default values

--- functions/main.tf
+++ functions/main.tf
@@ -7,6 +7,7 @@ variable "project_id" {}
 variable "project" {}
 variable "region" {}
 variable "secret_ids" {}
+variable "deploy_sa_email" {}
 
 module "cloud_function" {
   source   = "../modules/cloud-function"
@@ -14,15 +15,17 @@ module "cloud_function" {
 
   name                         = each.value.name
   description                  = each.value.description
-  project                      = var.project
-  runtime                      = lookup(each.value.cloud-function, "runtime", "python39")
   source_dir                   = "${path.module}/${each.value.name}"
-  execution_timeout            = lookup(each.value.cloud-function, "timeout", "30")
-  trigger_http                 = lookup(each.value.cloud-function, "trigger_http", true)
-  available_memory_mb          = lookup(each.value.cloud-function, "available_memory_mb", "128")
+  runtime                      = lookup(each.value.cloud-function, "runtime", null)
+  execution_timeout            = lookup(each.value.cloud-function, "timeout", null)
+  trigger_http                 = lookup(each.value.cloud-function, "trigger_http", null)
+  available_memory_mb          = lookup(each.value.cloud-function, "available_memory_mb", null)
   environment_variables        = lookup(each.value.cloud-function, "environment_variables", null)
   secret_environment_variables = lookup(each.value.cloud-function, "secrets", [])
-  secret_ids                   = var.secret_ids
+  # passing the static values
+  project         = var.project
+  secret_ids      = var.secret_ids
+  deploy_sa_email = var.deploy_sa_email
 }
 
 module "cloud_function_gen2" {
@@ -31,15 +34,17 @@ module "cloud_function_gen2" {
 
   name                         = each.value.name
   description                  = each.value.description
-  project                      = var.project
-  runtime                      = lookup(each.value.cloud-function-gen2, "runtime", "python39")
   source_dir                   = "${path.module}/${each.value.name}"
-  execution_timeout            = lookup(each.value.cloud-function-gen2, "timeout", "30")
-  trigger_http                 = lookup(each.value.cloud-function-gen2, "trigger_http", true)
-  available_memory_mb          = lookup(each.value.cloud-function-gen2, "available_memory_mb", "128")
+  runtime                      = lookup(each.value.cloud-function-gen2, "runtime", null)
+  execution_timeout            = lookup(each.value.cloud-function-gen2, "timeout", null)
+  trigger_http                 = lookup(each.value.cloud-function-gen2, "trigger_http", null)
+  available_memory_mb          = lookup(each.value.cloud-function-gen2, "available_memory_mb", null)
   environment_variables        = lookup(each.value.cloud-function-gen2, "environment_variables", null)
   secret_environment_variables = lookup(each.value.cloud-function-gen2, "secrets", [])
-  secret_ids                   = var.secret_ids
+  # passing the static values
+  project         = var.project
+  secret_ids      = var.secret_ids
+  deploy_sa_email = var.deploy_sa_email
 }
 
 module "cronjob" {
@@ -48,14 +53,16 @@ module "cronjob" {
 
   name                 = each.value.name
   description          = each.value.description
-  schedule             = lookup(each.value.cron, "schedule", "")
-  time_zone            = lookup(each.value.cron, "time_zone", "")
-  attempt_deadline     = lookup(each.value.cron, "attempt_deadline", "")
-  http_method          = lookup(each.value.cron, "http_method", "")
-  target_project       = var.project
-  target_region        = var.region
+  schedule             = lookup(each.value.cron, "schedule", null)
+  time_zone            = lookup(each.value.cron, "time_zone", null)
+  attempt_deadline     = lookup(each.value.cron, "attempt_deadline", null)
+  http_method          = lookup(each.value.cron, "http_method", null)
   target_function_name = module.cloud_function[each.value.name].function_name
   https_trigger_url    = module.cloud_function[each.value.name].function_trigger_url
+  # passing the static values
+  target_project  = var.project
+  target_region   = var.region
+  deploy_sa_email = var.deploy_sa_email
 
   depends_on = [
     module.cloud_function
@@ -68,14 +75,16 @@ module "cronjob-gen2" {
 
   name                 = each.value.name
   description          = each.value.description
-  schedule             = lookup(each.value.cron, "schedule", "")
-  time_zone            = lookup(each.value.cron, "time_zone", "")
-  attempt_deadline     = lookup(each.value.cron, "attempt_deadline", "")
-  http_method          = lookup(each.value.cron, "http_method", "")
-  target_project       = var.project
-  target_region        = var.region
+  schedule             = lookup(each.value.cron, "schedule", null)
+  time_zone            = lookup(each.value.cron, "time_zone", null)
+  attempt_deadline     = lookup(each.value.cron, "attempt_deadline", null)
+  http_method          = lookup(each.value.cron, "http_method", null)
   target_function_name = module.cloud_function_gen2[each.value.name].function_name
   https_trigger_url    = module.cloud_function_gen2[each.value.name].function_trigger_url
+  # passing the static values
+  target_project = var.project
+  target_region  = var.region
+  deploy_sa_email = var.deploy_sa_email
 
   depends_on = [
     module.cloud_function_gen2
