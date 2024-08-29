61a15a54474043f7cb985d3ad146ee00dd2defa3
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2024-08-29 11:19:59 -0700

    pass deploy service account

--- index.tf
+++ index.tf
@@ -1,10 +1,11 @@
 module "functions" {
   source = "./functions"
 
-  project    = local.project
-  region     = local.region
-  project_id = local.project_id
-  secret_ids = module.infrastructure.secret_ids
+  project         = local.project
+  region          = local.region
+  project_id      = local.project_id
+  secret_ids      = module.infrastructure.secret_ids
+  deploy_sa_email = module.infrastructure.deploy_sa_email
 
   depends_on = [
     module.infrastructure
